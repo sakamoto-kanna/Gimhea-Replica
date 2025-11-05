0200d42b863cd8389323579f00b615ccb146d564
sakamoto-kanna <kimgmg9136@gmail.com>
2025-11-05 17:17:43 +0900

    bugfix

--- js/carousel.js
+++ js/carousel.js
@@ -1,6 +1,21 @@
 const warpperImage = 1920;
-
+let mainIdx = 0;
+let mainCountIdx = 3;
 function carousel(step) {
-  const main = document.getElementById("main-warpper");
-  main.style.transform = `translate3d(-${warpperWidth * step}px,0,0)`;
+  const main = document.getElementById("carousel-warpper");
+  main.style.transform = `translate3d(-${warpperImage * step}px,0,0)`;
+}
+function mainPrev() {
+  if (mainIdx - 1 === -1) {
+    mainIdx = mainCountIdx;
+  }
+  mainIdx--;
+  carousel(mainIdx);
+}
+function mainNext() {
+  if (mainIdx + 1 === mainCountIdx) {
+    mainIdx = -1;
+  }
+  mainIdx++;
+  carousel(mainIdx);
 }
